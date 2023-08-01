76734ca3f701b248a87786a435a28a14b8d43af5
Chris Armbruster <chris@armbrustermail.com>
2023-08-01 14:40:28 -0500

    chore (back-end): product data model & api routes

--- server/db/Product.js
+++ server/db/Product.js
@@ -1,18 +1,90 @@
+// const { ENUM } = require('sequelize');
 const conn = require('./conn');
-const { STRING, UUID, UUIDV4 } = conn.Sequelize;
+const { UUID, UUIDV4, STRING, ENUM, DECIMAL, BOOLEAN } = conn.Sequelize;
 
 const Product = conn.define('product', {
   id: {
     type: UUID,
     primaryKey: true,
-    defaultValue: UUIDV4
+    required: true,
+    allowNull: false,
+    unique: true,
+    defaultValue: UUIDV4,
   },
   name: {
     type: STRING,
+    required: true,
     allowNull: false,
     validate: {
-      notEmpty: true
-    }
+      notEmpty: true,
+    },
+  },
+  category: {
+    type: ENUM('Mugs', 'T-shirts', 'Hats'),
+    allowNull: false,
+    required: true,
+    validate: { notEmpty: true },
+  },
+  description: {
+    type: STRING(1000),
+    allowNull: false,
+    required: true,
+    unique: false,
+    validate: { notEmpty: true },
+  },
+  sizeOptions: {
+    type: STRING,
+    allowNull: true,
+    required: false,
+    unique: false,
+  },
+  color: {
+    type: ENUM(
+      'Red',
+      'Blue',
+      'Green',
+      'Yellow',
+      'Orange',
+      'Purple',
+      'Black',
+      'White',
+      'Grey',
+      'Pink',
+      'Brown',
+      'Multi-Color'
+    ),
+    allowNull: true,
+    required: false,
+    unique: false,
+  },
+  gender: {
+    type: ENUM('Mens', 'Womens', 'Unisex'),
+    allowNull: true,
+    required: false,
+    unique: false,
+  },
+  imageUrl1: {
+    type: STRING,
+    defaultValue: '/images/sample_image.png',
+  },
+  price: {
+    type: DECIMAL(9, 2),
+    required: true,
+  },
+  salePrice: {
+    type: DECIMAL(9, 2),
+    required: false,
+  },
+  rating: {
+    type: DECIMAL(2, 1),
+  },
+  isFeatured: {
+    type: BOOLEAN,
+    defaultValue: false,
+  },
+  onSale: {
+    type: BOOLEAN,
+    defaultValue: false,
   },
 });
 
--- server/db/index.js
+++ server/db/index.js
@@ -2,45 +2,66 @@ const conn = require('./conn');
 const User = require('./User');
 const Product = require('./Product');
 const Order = require('./Order');
-const LineItem  = require('./LineItem');
+const LineItem = require('./LineItem');
 
 Order.belongsTo(User);
 LineItem.belongsTo(Order);
 Order.hasMany(LineItem);
 LineItem.belongsTo(Product);
 
-const syncAndSeed = async()=> {
+const syncAndSeed = async () => {
   await conn.sync({ force: true });
   const [moe, lucy, larry, foo, bar, bazz, ethyl] = await Promise.all([
     User.create({ username: 'moe', password: '123' }),
     User.create({ username: 'lucy', password: '123' }),
     User.create({ username: 'larry', password: '123' }),
-    Product.create({ name: 'foo' }),
-    Product.create({ name: 'bar' }),
-    Product.create({ name: 'bazz' }),
-    User.create({ username: 'ethyl', password: '123' }),
+    Product.create({
+      name: 'Foo',
+      category: 'Mugs',
+      description: 'Awesome foo mug.',
+      color: 'Red',
+      price: 10.0,
+    }),
+    Product.create({
+      name: 'Bar',
+      category: 'T-shirts',
+      description: 'Awesome bar t-shirt.',
+      color: 'Blue',
+      price: 20.0,
+      sizeOptions: 'S,M,L,XL',
+      gender: 'Unisex',
+    }),
+    Product.create({
+      name: 'Bazz',
+      category: 'Hats',
+      description: 'Awesome bazz hat.',
+      color: 'Green',
+      price: 15.0,
+      sizeOptions: 'One Size',
+      gender: 'Unisex',
+    }),
+    User.create({ username: 'ethyl', password: '123', isAdmin: true }),
   ]);
 
   const cart = await ethyl.getCart();
-  await ethyl.addToCart({ product: bazz, quantity: 3});
-  await ethyl.addToCart({ product: foo, quantity: 2});
+  await ethyl.addToCart({ product: bazz, quantity: 3 });
+  await ethyl.addToCart({ product: foo, quantity: 2 });
   return {
     users: {
       moe,
       lucy,
-      larry
+      larry,
     },
     products: {
       foo,
       bar,
-      bazz
-    }
+      bazz,
+    },
   };
 };
-
 
 module.exports = {
   syncAndSeed,
   User,
-  Product
+  Product,
 };
