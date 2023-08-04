e7598f0a4203b70e37f91c15adc52344b7136c2f
Michael <mrmchughes27@gmail.com>
2023-08-03 21:10:23 -0400

    started working on Navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/">Born to Code</a>
+    </div>
+  </nav>;
 };
 
 export default Navbar;
