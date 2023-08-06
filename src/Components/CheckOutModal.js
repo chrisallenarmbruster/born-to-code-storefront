c0416ff185f1d4431cbf5423328c0ea8f702c5bf
joel <jejanov@gmail.com>
2023-08-06 13:22:38 -0600

    square refactor

--- src/Components/CheckOutModal.js
+++ src/Components/CheckOutModal.js
@@ -12,9 +12,11 @@ import {
   ButtonToolbar,
   Form,
 } from 'react-bootstrap';
-import Payment from './Payment';
+import MyPaymentForm from './Payment';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { updateQuantity, fetchCart } from '../store/cart';
+import { CreditCard, PaymentForm } from 'react-square-web-payments-sdk';
 
 function CheckOut(props) {
   const { cart } = props;
@@ -32,157 +34,197 @@ function CheckOut(props) {
   }
 
   return (
-    <>
-      <Button variant="primary" onClick={handleShow}>
-        Checkout
-      </Button>
+    <PaymentForm
+      /**
+       * Identifies the calling form with a verified application ID generated from
+       * the Square Application Dashboard.
+       */
+      applicationId="sandbox-sq0idb-9aiQYyJJerVQ_xS_gMVOmA"
+      /**
+       * Invoked when payment form receives the result of a tokenize generation
+       * request. The result will be a valid credit card or wallet token, or an error.
+       */
+      cardTokenizeResponseReceived={(token, buyer) => {
+        console.info({ token, buyer });
+      }}
+      /**
+       * This function enable the Strong Customer Authentication (SCA) flow
+       *
+       * We strongly recommend use this function to verify the buyer and reduce
+       * the chance of fraudulent transactions.
+       */
+      createVerificationDetails={() => ({
+        amount: '1.00',
+        /* collected from the buyer */
+        billingContact: {
+          addressLines: ['123 Main Street', 'Apartment 1'],
+          familyName: 'Doe',
+          givenName: 'John',
+          countryCode: 'GB',
+          city: 'London',
+        },
+        currencyCode: 'GBP',
+        intent: 'CHARGE',
+      })}
+      /**
+       * Identifies the location of the merchant that is taking the payment.
+       * Obtained from the Square Application Dashboard - Locations tab.
+       */
+      locationId="LFJJJ47TKFPE2"
+    >
+      <>
+        <Button variant="primary" onClick={handleShow}>
+          Checkout
+        </Button>
 
-      <Modal
-        show={show}
-        onHide={handleClose}
-        size="lg"
-        scrollable={true}
-        animation={true}
-        contentClassName="modal-height"
-      >
-        <Modal.Header closeButton>
-          <Modal.Title>Checkout</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Container>
-            <Row>Your Cart:</Row>
-            {lineItems.map((item) => {
-              return (
-                <Card
-                  style={{ height: '3rem', border: 0 }}
-                  key={item.product.id}
-                >
-                  <Card.Body border={0} className="square border-bottom">
-                    <Row>
-                      <Col sm={6}>
-                        <Card.Text style={{ fontSize: 12 }}>
-                          {item.product.name}
-                        </Card.Text>
-                      </Col>
-                      <Col sm={2}>
-                        <Card.Text style={{ fontSize: 12 }}>
-                          Quantity: {item.quantity}
-                        </Card.Text>
-                      </Col>
-                      <Col sm={2}>
-                        <Card.Text style={{ fontSize: 12 }}>
-                          Price: ${item.product.price}
-                        </Card.Text>
-                      </Col>
-                      <Col sm={2}>
-                        <Button
-                          variant="secondary"
-                          size="sm"
-                          onClick={() =>
-                            handleRemove(cart, item.product, item.quantity)
-                          }
-                        >
-                          Remove
-                        </Button>
-                      </Col>
-                    </Row>
-                  </Card.Body>
-                </Card>
-              );
-            })}
-            <Card style={{ height: '3rem', border: 0 }}>
-              <Card.Body border={0} className="square border-bottom">
-                <Row>Where should we send your order?</Row>
-                <Row>
-                  <Col sm={6}>
-                    <Form>
-                      <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>First Name</Form.Label>
-                        <Form.Control
-                          type="name"
-                          placeholder="Enter first name"
-                        />
-                      </Form.Group>
-                    </Form>
-                  </Col>
-                  <Col sm={6}>
-                    <Form>
-                      <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Last Name</Form.Label>
-                        <Form.Control
-                          type="name"
-                          placeholder="Enter last name"
-                        />
-                      </Form.Group>
-                    </Form>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col sm={6}>
-                    <Form>
-                      <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Address</Form.Label>
-                        <Form.Control
-                          type="address"
-                          placeholder="Enter address"
-                        />
-                      </Form.Group>
-                    </Form>
-                  </Col>
-                  <Col sm={6}>
-                    <Form>
-                      <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>City</Form.Label>
-                        <Form.Control type="city" placeholder="Enter city" />
-                      </Form.Group>
-                    </Form>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col sm={6}>
-                    <Form>
-                      <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>State</Form.Label>
-                        <Form.Control type="State" placeholder="Enter state" />
-                      </Form.Group>
-                    </Form>
-                  </Col>
-                  <Col sm={6}>
-                    <Form>
-                      <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Zip Code</Form.Label>
-                        <Form.Control type="Zip" placeholder="Enter zip code" />
-                      </Form.Group>
-                    </Form>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col sm={6}>
-                    <Form>
-                      <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Phone Number</Form.Label>
-                        <Form.Control
-                          type="Phone"
-                          placeholder="Enter phone number"
-                        />
-                      </Form.Group>
-                    </Form>
-                  </Col>
-                </Row>
-              </Card.Body>
-            </Card>
-            {/* <Card style={{ height: '3rem', border: 0 }}>
-            <Card.Body border={0} className="square border-bottom">
-              <Payment />
-            </Card.Body>
-          </Card> */}
-          </Container>
-        </Modal.Body>
-        <Modal.Footer>
-          <Payment />
-        </Modal.Footer>
-      </Modal>
-    </>
+        <Modal
+          show={show}
+          onHide={handleClose}
+          size="lg"
+          scrollable={true}
+          animation={true}
+          contentClassName="modal-height"
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Checkout</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Container>
+              <Row>Your Cart:</Row>
+              {lineItems.map((item) => {
+                return (
+                  <Card
+                    style={{ height: '3rem', border: 0 }}
+                    key={item.product.id}
+                  >
+                    <Card.Body border={0} className="square border-bottom">
+                      <Row>
+                        <Col sm={6}>
+                          <Card.Text style={{ fontSize: 12 }}>
+                            {item.product.name}
+                          </Card.Text>
+                        </Col>
+                        <Col sm={2}>
+                          <Card.Text style={{ fontSize: 12 }}>
+                            Quantity: {item.quantity}
+                          </Card.Text>
+                        </Col>
+                        <Col sm={2}>
+                          <Card.Text style={{ fontSize: 12 }}>
+                            Price: ${item.product.price}
+                          </Card.Text>
+                        </Col>
+                        <Col sm={2}>
+                          <Button
+                            variant="secondary"
+                            size="sm"
+                            onClick={() =>
+                              handleRemove(cart, item.product, item.quantity)
+                            }
+                          >
+                            Remove
+                          </Button>
+                        </Col>
+                      </Row>
+                    </Card.Body>
+                  </Card>
+                );
+              })}
+              <Card style={{ height: '3rem', border: 0 }}>
+                <Card.Body border={0} className="square border-bottom">
+                  <Row>Where should we send your order?</Row>
+                  <Row>
+                    <Col sm={6}>
+                      <Form>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                          <Form.Label>First Name</Form.Label>
+                          <Form.Control
+                            type="name"
+                            placeholder="Enter first name"
+                          />
+                        </Form.Group>
+                      </Form>
+                    </Col>
+                    <Col sm={6}>
+                      <Form>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                          <Form.Label>Last Name</Form.Label>
+                          <Form.Control
+                            type="name"
+                            placeholder="Enter last name"
+                          />
+                        </Form.Group>
+                      </Form>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col sm={6}>
+                      <Form>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                          <Form.Label>Address</Form.Label>
+                          <Form.Control
+                            type="address"
+                            placeholder="Enter address"
+                          />
+                        </Form.Group>
+                      </Form>
+                    </Col>
+                    <Col sm={6}>
+                      <Form>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                          <Form.Label>City</Form.Label>
+                          <Form.Control type="city" placeholder="Enter city" />
+                        </Form.Group>
+                      </Form>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col sm={6}>
+                      <Form>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                          <Form.Label>State</Form.Label>
+                          <Form.Control
+                            type="State"
+                            placeholder="Enter state"
+                          />
+                        </Form.Group>
+                      </Form>
+                    </Col>
+                    <Col sm={6}>
+                      <Form>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                          <Form.Label>Zip Code</Form.Label>
+                          <Form.Control
+                            type="Zip"
+                            placeholder="Enter zip code"
+                          />
+                        </Form.Group>
+                      </Form>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col sm={6}>
+                      <Form>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                          <Form.Label>Phone Number</Form.Label>
+                          <Form.Control
+                            type="Phone"
+                            placeholder="Enter phone number"
+                          />
+                        </Form.Group>
+                      </Form>
+                    </Col>
+                  </Row>
+                </Card.Body>
+              </Card>
+            </Container>
+          </Modal.Body>
+          <Modal.Footer>
+            <MyPaymentForm />
+          </Modal.Footer>
+        </Modal>
+      </>
+    </PaymentForm>
   );
 }
 
@@ -192,4 +234,11 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(CheckOut);
+const mapDispatchToProps = (dispatch, { history }) => {
+  return {
+    updateQuantity: (item) => dispatch(updateQuantity(item, history)),
+    fetchCart: () => dispatch(fetchCart()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CheckOut);
