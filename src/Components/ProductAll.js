4e4f8c37adaa07e976006b70ccc1bb8f7a98c857
Chris Armbruster <chris@armbrustermail.com>
2023-08-07 10:00:40 -0500

    feat(front-end) Add Pagination to Product Catalog

--- src/Components/ProductAll.js
+++ src/Components/ProductAll.js
@@ -6,13 +6,30 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Spinner from 'react-bootstrap/Spinner';
 import ProductCatalogItemCard from './ProductCatalogItemCard';
+import Pagination from './Pagination';
 
 export class ProductAll extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPage: 1,
+      recordsPerPage: 6,
+    };
+  }
+
   componentDidMount() {
     this.props.setProducts();
   }
 
   render() {
+    const indexOfLastRecord =
+      this.state.currentPage * this.state.recordsPerPage;
+    const indexOfFirstRecord = indexOfLastRecord - this.state.recordsPerPage;
+    const currentRecords = this.props.products.slice(
+      indexOfFirstRecord,
+      indexOfLastRecord
+    );
+
     return (
       <Container className="mt-4">
         {this.props.isLoading ? (
@@ -22,13 +39,31 @@ export class ProductAll extends Component {
             </Spinner>
           </div>
         ) : (
-          <Row xs={1} md={2} lg={3} className="g-5">
-            {this.props.products.map((product) => (
-              <Col key={product.id} className="g-5">
-                <ProductCatalogItemCard product={product} />
-              </Col>
-            ))}
-          </Row>
+          <React.Fragment>
+            <Row xs={1} md={2} lg={3} className="g-5">
+              {currentRecords.map((product) => (
+                <Col key={product.id} className="g-5">
+                  <ProductCatalogItemCard product={product} />
+                </Col>
+              ))}
+            </Row>
+            <React.Fragment>
+              {this.props.products.length > this.state.recordsPerPage ? (
+                <div className="pagination justify-content-center flex-wrap mt-5">
+                  <Pagination
+                    recordsPerPage={this.state.recordsPerPage}
+                    totalRecords={this.props.products.length}
+                    paginate={(pageNumber) =>
+                      this.setState({ ...this.state, currentPage: pageNumber })
+                    }
+                    currentPage={this.state.currentPage}
+                  />
+                </div>
+              ) : (
+                ''
+              )}
+            </React.Fragment>
+          </React.Fragment>
         )}
       </Container>
     );
