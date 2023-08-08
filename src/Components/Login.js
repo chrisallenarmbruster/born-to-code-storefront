0f845c3e2ad282963dde3f671eab322e20584c7b
Michael <mrmchughes27@gmail.com>
2023-08-08 15:07:52 -0400

    reworked input field on Loginpage

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { attemptLogin, logout } from '../store';
 import { connect } from 'react-redux';
 import Button from 'react-bootstrap/Button'
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
 
 //user should be able to register with an email
 //username should be an email address
@@ -42,7 +44,7 @@ class Login extends Component {
               <h2>Welcome {this.props.auth.username}!</h2>
             </div>
             <div>
-              <button onClick={this.props.logout}>Logout</button>
+              <Button onClick={this.props.logout}>Logout</Button>
             </div>
           </div>
         ) : (
@@ -50,45 +52,34 @@ class Login extends Component {
             <h2>Login</h2>
             <Button variant="primary">Login</Button>
             <Button variant="secondary">Register</Button>
-            <p>Sign in with Username and Password</p>
-            <form onSubmit={login}>
-              <input
-                placeholder="Username"
-                value={credentials.username}
-                name="username"
-                onChange={onChange}
-              />
-              <input
-                placeholder="password"
-                name="password"
-                value={credentials.password}
-                onChange={onChange}
-              />
-              <button>Login</button>
-            </form>
+
+            <Form.Label htmlFor="SignIn">Sign in with Email and Password</Form.Label>
+            <Form onSubmit={login}>
+              <FloatingLabel controlId="floatingUsername" label="Email">
+                <Form.Control type="text" placeholder="Email" value={credentials.username} name="username" onChange={onChange}/>
+              </FloatingLabel>
+
+              <FloatingLabel controlId="floatingPassword" label="Password">
+                <Form.Control type="password" placeholder="Password" value={credentials.password} name="password" onChange={onChange}/>
+              </FloatingLabel>
+              
+              <Button type="submit">Login</Button>
+            </Form>
             
-            <p>Sign up with Username and Password</p>
-            <form>
-              <input
-                placeholder="Username"
-                value={""}
-                name="username"
-                onChange={onChange}
-              />
-              <input
-                placeholder="Password"
-                name="Password"
-                value={""}
-                onChange={onChange}
-              />
-              <input
-                placeholder="Repeat Password"
-                name="Repeat Password"
-                value={""}
-                onChange={onChange}
-              />
-              <button>Sign Up</button>
-            </form>
+            <Form.Label htmlFor="SignUp">Sign up with Username and Password</Form.Label>
+            <Form>
+              <FloatingLabel controlId="floatingUsername" label="Email">
+                <Form.Control type="email" placeholder="Email" name="email" value={""} onChange={onChange}/>
+              </FloatingLabel>
+              <FloatingLabel controlId="floatingPassword" label="Password">
+                <Form.Control type="password" placeholder="Password" name="password" value={""} onChange={onChange}/>
+              </FloatingLabel>
+              <FloatingLabel controlId="floatingRepeatPassword" label="Repeat Password">
+                <Form.Control type="password" placeholder="Repeat Password" name="repeat_password" value={""} onChange={onChange}/>
+              </FloatingLabel>
+             
+              <Button>Sign Up</Button>
+            </Form>
           </div>
         )}
       </div>
