68354c26d51ee5d2e06aa6383c83e5999ff55ec2
joel <jejanov@gmail.com>
2023-08-13 06:02:39 -0600

    added redux dev tools

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import auth from './auth';
@@ -15,7 +15,18 @@ const reducer = combineReducers({
   ordersSingleUser,
 });
 
-const store = createStore(reducer, applyMiddleware(thunk, logger));
+// Compose enhancers with trace option enabled
+const composeEnhancers =
+  typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        trace: true,
+        traceLimit: 25,
+      })
+    : compose;
+
+const enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+
+const store = createStore(reducer, enhancer);
 
 export default store;
 
